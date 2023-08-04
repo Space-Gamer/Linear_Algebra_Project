d3e7f35eb77dbfe438e113451f0003b0516b8eeb
Space_Gamer <spacegamer1111@gmail.com>
2023-08-04 16:44:48 +0530

    Cyclohexane with single unit along with their distance matrices. Also has cyclohexane without two intermediate nodes.

--- Chemical Structures/cyclohexane_without_intermediate_pts.py
+++ Chemical Structures/cyclohexane_without_intermediate_pts.py
@@ -47,30 +47,30 @@ def concat_graphs(adj_mat1, adj_mat2):
 
 def create_cyclohexane_ring(start_node):
     node_lst = [i for i in range(start_node, start_node + 8)]
-    cyclohexane_mat = np.zeros((8, 8))
+    cyclohexane_mat = np.zeros((6, 6))
     for i in range(6):
         cyclohexane_mat[i][(i + 1) % 6] = 1
         cyclohexane_mat[(i + 1) % 6][i] = 1
 
     # internode 1,2
-    cyclohexane_mat[1][6] = 1
-    cyclohexane_mat[6][1] = 1
-    cyclohexane_mat[2][6] = 1
-    cyclohexane_mat[6][2] = 1
-    cyclohexane_mat[4][6] = 1
-    cyclohexane_mat[6][4] = 1
-    cyclohexane_mat[5][6] = 1
-    cyclohexane_mat[6][5] = 1
+    # cyclohexane_mat[1][6] = 1
+    # cyclohexane_mat[6][1] = 1
+    # cyclohexane_mat[2][6] = 1
+    # cyclohexane_mat[6][2] = 1
+    # cyclohexane_mat[4][6] = 1
+    # cyclohexane_mat[6][4] = 1
+    # cyclohexane_mat[5][6] = 1
+    # cyclohexane_mat[6][5] = 1
 
     # internode 2,3
-    cyclohexane_mat[2][7] = 1
-    cyclohexane_mat[7][2] = 1
-    cyclohexane_mat[3][7] = 1
-    cyclohexane_mat[7][3] = 1
-    cyclohexane_mat[5][7] = 1
-    cyclohexane_mat[7][5] = 1
-    cyclohexane_mat[0][7] = 1
-    cyclohexane_mat[7][0] = 1
+    # cyclohexane_mat[2][7] = 1
+    # cyclohexane_mat[7][2] = 1
+    # cyclohexane_mat[3][7] = 1
+    # cyclohexane_mat[7][3] = 1
+    # cyclohexane_mat[5][7] = 1
+    # cyclohexane_mat[7][5] = 1
+    # cyclohexane_mat[0][7] = 1
+    # cyclohexane_mat[7][0] = 1
 
     # print(dist_mat(cyclohexane_mat).max(axis=None))
 
@@ -88,26 +88,26 @@ def main():
 
     # Connecting individual rings
 
-    # 0, 8
-    adj_mat[0][8] = 1
-    adj_mat[8][0] = 1
+    # 0, 6
+    adj_mat[0][6] = 1
+    adj_mat[6][0] = 1
 
-    # 4, 30
-    adj_mat[4][30] = 1
-    adj_mat[30][4] = 1
+    # 4, 22
+    adj_mat[4][22] = 1
+    adj_mat[22][4] = 1
 
-    # 12, 20
-    adj_mat[12][20] = 1
-    adj_mat[20][12] = 1
-
-    # 16, 24
-    adj_mat[16][24] = 1
-    adj_mat[24][16] = 1
+    # 10, 16
+    adj_mat[10][16] = 1
+    adj_mat[16][10] = 1
+    
+    # 12, 18
+    adj_mat[12][18] = 1
+    adj_mat[18][12] = 1
 
     draw_graph(adj_mat)
 
-    print("Distance Matrix written to file 'cyclohexane_dist_mat.txt'")
-    np.savetxt('cyclohexane_dist_mat.txt', dist_mat(adj_mat), fmt='%d')
+    print("Distance Matrix written to file 'cyclohexane_dist_mat_without_intermediate_pts.txt'")
+    np.savetxt('cyclohexane_dist_mat_without_intermediate_pts.txt', dist_mat(adj_mat), fmt='%d')
 
     print("Max distance between any two nodes:")
     print(dist_mat(adj_mat).max(axis=None))
